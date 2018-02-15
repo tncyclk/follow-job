7b79e85e5e219e91e699d16cf1913bc69b01947c
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2018-02-15 17:17:12 +0300

    bugfix

--- follow/urls.py
+++ follow/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('<slug>/', job_detail, name="detail"),
     path('<slug>/update/', job_update, name="update"),
     path('<slug>/delete/', job_delete, name="delete"),
-    path('form/', form_view, name="form"),
-    path('login/', login_view, name="login"),
-    path('logout/', logout_view, name="logout"),
-    path('project/', project_index, name="project"),
+    path('form', form_view, name="form"),
+    path('login', login_view, name="login"),
+    path('logout', logout_view, name="logout"),
+    path('project', project_index, name="project"),
 
 ]
